bb6d076f10683665c041fd0dc99d62a16d830d35
Kenny Johansson <wirehell@gmail.com>
2023-04-08 14:11:53 +0200

    Add a simple README

---
+++ README.md
@@ -0,0 +1,22 @@
+# OpenP1
+
+Open source reader for (mainly) Swedish electricity meters with P1 port.
+
+Exposes data read from meter as a Modbus TCP or UDP server on a Thread network.
+
+The software is in an early stage, and usage is not recommended unless you know what you are doing.
+
+# Suggested Circuit
+
+TODO
+
+# Suggested way of collecting and presenting data
+
+Home Assistant with Modbus integration. 
+Sample config TODO
+
+
+# References
+
+https://www.energiforetagen.se/globalassets/energiforetagen/det-erbjuder-vi/kurser-och-konferenser/elnat/branschrekommendation-lokalt-granssnitt-v2_0-201912.pdf
+https://hanporten.se/uit
--- tests/src/test_blep1.c
+++ tests/src/test_blep1.c
@@ -2,18 +2,17 @@
 
 #include <zephyr/ztest.h>
 
-#include "lib/ble_definitions.h"
-#include "lib/blep1.h"
+#include "lib/openp1.h"
 
-ZTEST_SUITE(blep1_suite, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(openp1_suite, NULL, NULL, NULL, NULL, NULL);
 
-ZTEST(blep1_suite, test_definitions_for_all_items) {
+ZTEST(openp1_suite, test_definitions_for_all_items) {
 	for (int i = 0 ; i < _ITEM_COUNT ; i++) {
 		zassert_equal(data_definition_table[i].item, i);
 	}
 }
 
-ZTEST(blep1_suite, test_ble_definitions_for_all_items) {
+ZTEST(openp1_suite, test_ble_definitions_for_all_items) {
 	for (int i = 0 ; i < _ITEM_COUNT ; i++) {
 		zassert_equal(gatt_ch_table[i].item, i);
 	}
